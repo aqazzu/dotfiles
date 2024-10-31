ee79dfad0c747df704ef71adec1f7838820e3969
aqazzu <aqazzu@posteo.com>
2024-10-31 01:08:05 +0100

    Added new shortcuts to nvim: * tab creating and movement shortcuts * alternative to <ESC>: <A-q>

--- .config/nvim/lua/config/keym.lua
+++ .config/nvim/lua/config/keym.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>tn",vim.cmd.tabnew)
+vim.keymap.set("n", "<leader>tc",vim.cmd.tabclose)
+vim.keymap.set("n", "<leader>tl",vim.cmd.tabnext)
+vim.keymap.set("n", "<leader>th",vim.cmd.tabprevious)
+
+vim.keymap.set({"n", "v", "i"}, "<A-q>", "<ESC>")
