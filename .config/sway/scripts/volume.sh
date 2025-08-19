c4b6d404cfb3fa9586facb2a215697bbf0ee9f58
aqazzu <aqazzu@posteo.com>
2025-08-19 02:59:36 +0200

    Added upper limit to volume script to protect my poor aging ears...

--- .config/sway/scripts/volume.sh
+++ .config/sway/scripts/volume.sh
@@ -4,8 +4,20 @@ VALUE=$1
 WOBSOCK=$XDG_RUNTIME_DIR/wob.sock
 
 get_vol(){
-    wpctl get-volume @DEFAULT_SINK@ | tr -d . | cut -d" " -f2
+    raw_vol=$(wpctl get-volume @DEFAULT_SINK@ | tr -d . | cut -d" " -f2)
+    if [ "$raw_vol" -lt 100 ]; then
+        echo "$raw_vol" | cut -c 2-
+    else
+        echo "$raw_vol"
+    fi
 }
 
-pactl set-sink-volume @DEFAULT_SINK@ $VALUE
+OLD_VOL=$(get_vol)
+CLEAN_VALUE=$(echo "$VALUE" | rev | cut -c 2- | rev)
+
+if [ $(( $OLD_VOL + $CLEAN_VALUE )) -le "100" ]; then
+    pactl set-sink-volume @DEFAULT_SINK@ $VALUE
+else
+    pactl set-sink-volume @DEFAULT_SINK@ 100%
+fi
 get_vol > $WOBSOCK
